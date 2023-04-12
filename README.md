22020c5aaa4981cf5bc9711cb5fc0e63202bc4f4
SM Sohag <sohagsheik32@gmail.com>
2023-04-12 17:46:39 +0600

    readme file impliment

---
+++ README.md
@@ -0,0 +1,3 @@
+### Circle Jobs
+
+# Featured Documentation
--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -4,9 +4,9 @@ const Blog = ({ blog }) => {
   const { author, question, answer } = blog;
   return (
     <article className="p-5 md:p-10">
-      <h3 className="text-xl font-extrabold opacity-50">{question}</h3>
-      <p className="ms-2 my-3">{answer}</p>
-      <p className="text-right">_{author}</p>
+      <h3 className="text-xl font-extrabold opacity-60">{question}</h3>
+      <p className="ms-2 my-3 opacity-60">{answer}</p>
+      <p className="text-right opacity-50">_{author}</p>
     </article>
   );
 };
