633fc0d630de5be8a6df36dd3dad29478f98199c
SM Sohag <sohagsheik32@gmail.com>
2023-04-12 14:40:05 +0600

    jobs json data update

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import { MapPinIcon, CurrencyDollarIcon } from "@heroicons/react/24/solid";
 const Job = ({ job }) => {
   const {
     id,
@@ -14,21 +14,30 @@ const Job = ({ job }) => {
   } = job;
   return (
     <div className="border p-10 rounded-sm">
-      <img src={companyLogo} alt="companyLogo" />
-      <h4>{jobTitle}</h4>
-      <p>{companyName}</p>
-      <div className="flex gap-3">
-        <span className="primary-text border py-1 px-2 border-blue-500">
+      <img
+        className="w-40 mb-2 h-12 object-contain"
+        src={companyLogo}
+        alt="companyLogo"
+      />
+      <h4 className="font-bold my-1">{jobTitle}</h4>
+      <p className="text-gray-700 text-sm">{companyName}</p>
+      <div className="flex my-3 gap-3">
+        <span className="primary-text border rounded-sm py-1 px-2 border-blue-500">
           {jobType}
         </span>
-        <span className="primary-text border py-1 px-1 border-blue-500">
+        <span className="primary-text border rounded-sm py-1 px-2 border-blue-500">
           {employmentStatus}
         </span>
       </div>
-      <div>
-        <p>
-          <span>icon</span>
-          {companyLocation} <span>icon</span>
+      <div className="mb-4">
+        <p className="flex text-sm items-center  md:flex-row gap-2">
+          <span>
+            <MapPinIcon className="h-4 w-4 text-gray-400" />
+          </span>
+          {companyLocation}
+          <span>
+            <CurrencyDollarIcon className="h-4 w-4 text-gray-400" />
+          </span>
           {salary}
         </p>
       </div>
