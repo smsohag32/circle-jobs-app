3a99b056f7db14ab8986980d474bd6ecf2d2f1ed
SM Sohag <sohagsheik32@gmail.com>
2023-04-12 15:51:20 +0600

    some style and layout update

--- src/components/JobDetails.jsx
+++ src/components/JobDetails.jsx
@@ -1,69 +1,89 @@
 import { useLoaderData, useParams } from "react-router-dom";
-import { PhoneIcon, MapPinIcon, AtSymbolIcon } from "@heroicons/react/24/solid";
+import {
+  PhoneIcon,
+  MapPinIcon,
+  AtSymbolIcon,
+  CurrencyDollarIcon,
+  IdentificationIcon,
+} from "@heroicons/react/24/solid";
 import { useContext } from "react";
 import { AppliedJobsContext } from "../App";
 import TitleBanner from "./TitleBanner";
 const JobDetails = () => {
   const jobDetail = useLoaderData();
   const { handleApplyJob } = useContext(AppliedJobsContext);
-  //   console.log(jobDetail);
+
   const {
     jobResponsibility,
+    jobTitle,
     jobDescription,
     experiences,
     educationalRequirements,
     contactInfo,
     companyLocation,
+    salary,
   } = jobDetail;
 
   return (
     <>
       <TitleBanner>Details Job</TitleBanner>
-      <div className="default-container py-16 grid grid-cols-1 md:grid-cols-3 gap-4">
+      <div className="default-container py-16 grid grid-cols-1 md:grid-cols-3  gap-4">
         <div className="col-span-1 md:col-span-2">
-          <p>
-            <span className="font-bold text-sm">Job Description:</span>{" "}
+          <p className="mb-5">
+            <span className="font-bold text-sm">Job Description: </span>
             <small>{jobDescription}</small>
           </p>
-          <p>
+          <p className="mb-5">
             <span className="font-bold text-sm">Job Responsibility: </span>
             <small>{jobResponsibility}</small>
           </p>
-          <p>
-            <span className="font-bold text-sm">Educational Requirement: </span>{" "}
+          <p className="mb-5">
+            <span className="font-bold mb-2 text-sm">
+              Educational Requirement:{" "}
+            </span>
             <br /> {educationalRequirements}
           </p>
-          <p>
+          <p className="mb-5">
             <span className="font-bold text-sm">Experiences: </span>
             <br />
             {experiences}
           </p>
         </div>
-        <div className="bg-gray-100 flex flex-col p-7">
-          <div className="">
-            <h5>Job Details</h5>
+        <div className=" flex flex-col">
+          <div className="bg-gray-100 p-7 rounded-sm">
+            <h5 className="pb-2">Job Details</h5>
             <hr />
-            <p></p>
-            <p></p>
-            <h5>Contact Information</h5>
-            <hr />
-            <p className="flex items-center gap-2">
+            <p className="flex items-center text-sm text-gray-700 my-2 gap-2">
               <span>
-                <PhoneIcon className="h-4 w-4 primary-text" />
+                <CurrencyDollarIcon className="h-4 w-4 opacity-70 primary-text" />
+              </span>
+              <span>Salary: {salary}</span>
+            </p>
+            <p className="flex items-center mb-2 text-sm text-gray-700 gap-2">
+              <span>
+                <IdentificationIcon className="h-4 w-4 opacity-70 primary-text" />
+              </span>
+              <span>Job Title: {jobTitle}</span>
+            </p>
+            <h5 className="py-2">Contact Information</h5>
+            <hr />
+            <p className="flex items-center my-2 text-sm text-gray-700 gap-2">
+              <span>
+                <PhoneIcon className="h-4 w-4 opacity-70 primary-text" />
               </span>
               <span>Phone: </span>
               {contactInfo.phone}
             </p>
-            <p className="flex items-center gap-2">
+            <p className="flex items-center my-2 text-sm text-gray-700 gap-2">
               <span>
-                <AtSymbolIcon className="h-4 w-4 primary-text" />
+                <AtSymbolIcon className="h-4 w-4 opacity-70 primary-text" />
               </span>
               <span>Email: </span>
               {contactInfo.email}
             </p>
-            <p className="flex items-center gap-2">
+            <p className="flex items-center my-2 text-sm text-gray-700 gap-2">
               <span>
-                <MapPinIcon className="h-4 w-4 primary-text" />
+                <MapPinIcon className="h-4 w-4 opacity-70 primary-text" />
               </span>
               <span>Address: </span>
               {companyLocation}
@@ -71,7 +91,7 @@ const JobDetails = () => {
           </div>
           <button
             onClick={() => handleApplyJob(jobDetail)}
-            className="primary-btn"
+            className="primary-btn mt-5"
           >
             Apply Now
           </button>
