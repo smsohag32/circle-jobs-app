3fdc3ba1791b6ffc98b08b9ad9af1a94a243cc6c
SM Sohag <sohagsheik32@gmail.com>
2023-04-12 15:56:28 +0600

    statistic page some style update

--- src/components/Statistic.jsx
+++ src/components/Statistic.jsx
@@ -11,6 +11,7 @@ import {
   Legend,
   ResponsiveContainer,
 } from "recharts";
+import TitleBanner from "./TitleBanner";
 
 // VerticalComposedChart data and my assignments
 let assignmentMarks = [
@@ -26,43 +27,46 @@ let assignmentMarks = [
 
 const Statistic = () => {
   return (
-    <div className="default-container mb-16 border-2 p-10 bg-gray-100">
-      <div className="text-xs max-w-[940px] mx-auto w-full h-80">
-        <ResponsiveContainer width="100%" height="100%">
-          <ComposedChart
-            layout="vertical"
-            width={1000}
-            height={400}
-            data={assignmentMarks}
-            margin={{
-              top: 80,
-              right: 20,
-              bottom: 20,
-              left: 80,
-            }}
-          >
-            <CartesianGrid stroke="#f5f5f5" />
-            <XAxis type="number" />
-            <YAxis
-              name="Assignment"
-              dataKey="name"
-              type="category"
-              scale="band"
-            />
-            <Tooltip />
-            <Legend />
-            {/* <Area dataKey="amt" fill="#8884d8" stroke="#8884d8" /> */}
-            <Bar
-              dataKey="marks"
-              name="Assignment Marks"
-              barSize={20}
-              fill="#413ea0"
-            />
-            <Line dataKey="marks" name="Marks" stroke="#ff7300" />
-          </ComposedChart>
-        </ResponsiveContainer>
+    <>
+      <TitleBanner>Assignments Statistic</TitleBanner>
+      <div className="default-container mb-16 p-8">
+        <div className="text-xs max-w-[940px] mx-auto w-full h-80">
+          <ResponsiveContainer width="100%" height="100%">
+            <ComposedChart
+              layout="vertical"
+              width={1000}
+              height={400}
+              data={assignmentMarks}
+              margin={{
+                top: 80,
+                right: 20,
+                bottom: 20,
+                left: 80,
+              }}
+            >
+              <CartesianGrid stroke="#f5f5f5" />
+              <XAxis type="number" />
+              <YAxis
+                name="Assignment"
+                dataKey="name"
+                type="category"
+                scale="band"
+              />
+              <Tooltip />
+              <Legend />
+              {/* <Area dataKey="amt" fill="#8884d8" stroke="#8884d8" /> */}
+              <Bar
+                dataKey="marks"
+                name="Assignment Marks"
+                barSize={20}
+                fill="#413ea0"
+              />
+              <Line dataKey="marks" name="Marks" stroke="#ff7300" />
+            </ComposedChart>
+          </ResponsiveContainer>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
